240cace4301f082fe8bf1b4d65123ff65f4e5f32
Paul Cooke <paulgfcooke@gmail.com>
2020-03-17 20:13:10 +0000

    nice little outline of a home page, button tha links to a test render of the tests page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import { createGlobalStyle } from 'styled-components'
 
 import Home from '../src/components/common/Home'
 import BestTests from './components/best-boy-tests/best-tests'

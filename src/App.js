42a598c5b40fa8e13cabe28020f260f892e105f4
Paul Cooke <paulgfcooke@gmail.com>
2020-03-17 17:32:09 +0000

    initial set up of best-boy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+  background-color: #282c34;  
+  min-height: 100vh;  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const Title = styled.h1`
+  color: red;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>
+          hello there people
+      </Title>
+    </Wrapper>
   );
 }
 

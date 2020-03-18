a3905e0a06b1f8e2958bb2bc877c66201f360799
Paul Cooke <paulgfcooke@gmail.com>
2020-03-18 15:23:32 +0000

    very basic render up on non create react app version

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+// browser router, switch and route from react router dom
+
+const App = () => (
+  
+  <h1>Testing</h1>
+  
+)
+
+
+
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+)

5df8d6b985eae7045ced2de0ccd6c66d04ffa946
Paul Cooke <paulgfcooke@gmail.com>
2020-03-28 19:32:36 +0000

    trying again

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/app.js',
@@ -34,9 +33,6 @@ module.exports = {
       template: 'src/index.html',
       filename: 'index.html',
       inject: 'body'
-    }),
-    new CopyWebpackPlugin([
-      { from: './src/assets', to: 'assets' }
-    ])
+    })
   ]
 }

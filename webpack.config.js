5440995e1608fff6e4cdc40a739c1a21d829dd3a
Paul Cooke <paulgfcooke@gmail.com>
2020-03-28 20:13:59 +0000

    trying again

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve('frontend/dist'),
+    path: path.resolve('dist'),
     publicPath: '/'
   },
   module: {
